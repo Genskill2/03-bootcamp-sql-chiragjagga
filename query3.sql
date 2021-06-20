ce3bb093af3faea46b4c7e0448d6a07e1d9c57b9
Chirag Jagga <cj@cbarena.net>
2021-06-20 14:48:34 +0530

    query3 updated

--- query3.sql
+++ query3.sql
@@ -1,1 +1,1 @@
-SELECT title FROM books,books_subjects,subjects WHERE books.id=books_subjects.book AND books_subjects.subject=subject.id AND subjects.name IN ("Technology" , "Politics");
+SELECT title FROM books,books_subjects,subjects WHERE books.id=books_subjects.book AND books_subjects.subject=subjects.id AND subjects.name IN ("Technology" , "Politics");
